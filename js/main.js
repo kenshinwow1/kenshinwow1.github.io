032f9d8f2215db6a335ea5067801d1b49f2035e9
kenshinwow1 <kenshinwow1@gmail.com>
2020-11-09 01:55:19 +0200

    commid

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
-
-
-
-    $('.heroes__slider-img').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        asNavFor: '.heroes__slider-text',
-        asNavFor: '.heroes__slider-img',
-        prevArrow: '<button class="slick-btm slick-next"><img src="images/стрелка.png" alt="prev"> </button>',
-        nextArrow: '<button class="slick-next slick-next"><img src="images/стрелка.png" alt="next"> </button>',
-    });
-    $('.heroes__slider-text').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-       asNavFor:'.heroes__slider-img',
-       fade:true,
-      });
- 
+$('.heroes__slider-img').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.heroes__slider-text',
+    asNavFor: '.heroes__slider-img',
+    prevArrow: '<button class="slick-btm slick-next"><img src="images/стрелка.png" alt="prev"> </button>',
+    nextArrow: '<button class="slick-next slick-next"><img src="images/стрелка.png" alt="next"> </button>',
+});
+$('.heroes__slider-text').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.heroes__slider-img',
+    fade: true,
+});
